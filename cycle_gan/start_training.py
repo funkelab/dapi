deea3403556cf8a53056806d5fb7dfe00e7ab218
Jan Funke <jfunke@iri.upc.edu>
2021-10-26 14:50:45 -0400

    Add cats and dogs

--- cycle_gan/start_training.py
+++ cycle_gan/start_training.py
@@ -16,12 +16,20 @@ def start_training(train_setup,
                    netG="resnet_9blocks",
                    in_size=128,
                    continue_train=False,
-                   submit_cmd="python"):
+                   submit_cmd="python",
+                   input_nc=1,
+                   output_nc=1,
+                   crop_size=None,
+                   preprocess="none"):
+
+    if crop_size is None:
+        crop_size = in_size
 
     for data_root in data_roots:
         base_cmd = f"{submit_cmd} -u cycle_gan/train.py" +\
-                   " --dataroot {} --name {} --input_nc 1 --output_nc 1 --netG {} --load_size {}"+\
-                   " --crop_size {} --checkpoints_dir {} --display_id 0"
+                   " --dataroot {} --name {} --input_nc {} --output_nc {} --netG {} --load_size {}"+\
+                   " --crop_size {} --checkpoints_dir {} --display_id 0"+\
+                   " --preprocess {}"
 
         if continue_train:
             base_cmd += " --continue_train"
@@ -35,12 +43,36 @@ def start_training(train_setup,
 
         cmd = base_cmd.format(data_root,
                               train_setup_name,
+                              input_nc,
+                              output_nc,
                               netG,
                               in_size,
                               in_size,
-                              checkpoint_dir)
+                              checkpoint_dir,
+                              preprocess)
         subprocess.Popen(cmd, 
                          shell=True) 
+
+def train_cats_dogs(train_setup, data_root, submit_cmd):
+    """
+    avg width:  404.09904 avg_height:  360.47808 avg_side:  382.288
+    use closest divisible by 4 -> 384
+    we use this as load size and crop down to 256
+    """
+    datasets = ["cats_dogs"]
+
+    data_roots = []
+    for d in datasets:
+        data_roots.append(f"{data_root}/{d}")
+
+    start_training(train_setup=train_setup, 
+                   data_roots=data_roots,
+                   in_size=384,
+                   crop_size=256,
+                   submit_cmd=submit_cmd,
+                   input_nc=3,
+                   output_nc=3,
+                   preprocess="resize_and_crop")
 
 def train_mnist(train_setup, data_root, submit_cmd):
     datasets = [f"{i}_{j}" for i,j in list(itertools.combinations([k for k in range(10)], 2))]
@@ -96,7 +128,9 @@ if __name__ == "__main__":
     exp_to_f = {"mnist": train_mnist,
                 "synapses": train_synapses,
                 "disc_a": train_disc_a,
-                "disc_b": train_disc_b}
+                "disc_b": train_disc_b,
+                "cats_dogs": train_cats_dogs}
+
     args = parser.parse_args()
     f_train = exp_to_f[args.experiment]
     f_train(args.setup, args.data_root, args.submit_cmd)

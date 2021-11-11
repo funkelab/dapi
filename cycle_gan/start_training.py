45b4cb7ca75390be5c4e74724a02f2a61e3c8e37
nilsec <nils.eckstein@googlemail.com>
2021-11-11 06:38:36 -0500

    Remove cats & dogs

--- cycle_gan/start_training.py
+++ cycle_gan/start_training.py
@@ -20,7 +20,14 @@ def start_training(train_setup,
                    input_nc=1,
                    output_nc=1,
                    crop_size=None,
-                   preprocess="none"):
+                   preprocess="none",
+                   batch_size=1,
+                   netD="basic",
+                   n_layers_D=3,
+                   lr=0.0002,
+                   lambda_A=10.0,
+                   lambda_B=10.0,
+                   lambda_id=0.5):
 
     if crop_size is None:
         crop_size = in_size
@@ -29,7 +36,9 @@ def start_training(train_setup,
         base_cmd = f"{submit_cmd} -u cycle_gan/train.py" +\
                    " --dataroot {} --name {} --input_nc {} --output_nc {} --netG {} --load_size {}"+\
                    " --crop_size {} --checkpoints_dir {} --display_id 0"+\
-                   " --preprocess {}"
+                   " --preprocess {} --batch_size {} --netD {}"+\
+                   " --n_layers_D {} --lr {} --lambda_A {} --lambda_B {}"+\
+                   " --lambda_identity {}"
 
         if continue_train:
             base_cmd += " --continue_train"
@@ -49,30 +58,16 @@ def start_training(train_setup,
                               in_size,
                               in_size,
                               checkpoint_dir,
-                              preprocess)
+                              preprocess,
+                              batch_size,
+                              netD,
+                              n_layers_D,
+                              lr,
+                              lambda_A,
+                              lambda_B,
+                              lambda_id)
         subprocess.Popen(cmd, 
                          shell=True) 
-
-def train_cats_dogs(train_setup, data_root, submit_cmd):
-    """
-    avg width:  404.09904 avg_height:  360.47808 avg_side:  382.288
-    use closest divisible by 4 -> 384
-    we use this as load size and crop down to 256
-    """
-    datasets = ["cats_dogs"]
-
-    data_roots = []
-    for d in datasets:
-        data_roots.append(f"{data_root}/{d}")
-
-    start_training(train_setup=train_setup, 
-                   data_roots=data_roots,
-                   in_size=384,
-                   crop_size=256,
-                   submit_cmd=submit_cmd,
-                   input_nc=3,
-                   output_nc=3,
-                   preprocess="resize_and_crop")
 
 def train_mnist(train_setup, data_root, submit_cmd):
     datasets = [f"{i}_{j}" for i,j in list(itertools.combinations([k for k in range(10)], 2))]
@@ -128,8 +123,7 @@ if __name__ == "__main__":
     exp_to_f = {"mnist": train_mnist,
                 "synapses": train_synapses,
                 "disc_a": train_disc_a,
-                "disc_b": train_disc_b,
-                "cats_dogs": train_cats_dogs}
+                "disc_b": train_disc_b}
 
     args = parser.parse_args()
     f_train = exp_to_f[args.experiment]
